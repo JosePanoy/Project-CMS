7379299f83e2844609c3741a90e95ad35bda656e
Jan Rasheed Calderon <calderon.janrasheedcit2011@gmail.com>
2024-09-20 16:30:43 +0800

    fixing the user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,3 +1,4 @@
+//user.routes.js
 import express from 'express';
 import { createUser, login, getUserInfo, getAllUsers, getUserById, getUserProfile } from '../controllers/user.controller.js';
 import { upload } from '../middleware/upload.js';
@@ -8,7 +9,7 @@ router.post('/register', upload.single('profilePic'), createUser);
 router.post('/login', login);
 router.get('/info', getUserInfo);
 router.get('/', getAllUsers);
-router.get('/:id', getUserById); // Changed to /:id for user by ID
-router.get('/profile/:id', getUserProfile); // Ensure this is kept
+router.get('/:id', getUserById); 
+router.get('/profile/:id', getUserProfile); 
 
 export default router;
